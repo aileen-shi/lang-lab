8e136dbe309c4bc5fb1bbcb9ff1e14e7b522259d
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2025-01-15 21:08:06 -0500

    Added footer
    Created footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
 const Footer = () => {
-  return <div></div>;
+  return (
+    <div className="w-screen py-6 bg-sky-600 text-center font-open-sans">
+      <h3 className="text-white tracking-widest">
+        Copyright © 2025 Lang Lab | Designed by Aileen Shi
+      </h3>
+    </div>
+  );
 };
 export default Footer;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import Carousel from "../components/Carousel";
 import Navbar from "../components/Navbar";
 import HeroText from "../components/HeroText";
+import Footer from "../components/Footer";
 import slide1 from "../assets/images/Slide1.png";
 import slide2 from "../assets/images/Slide2.png";
 import slide3 from "../assets/images/Slide3.png";
@@ -30,6 +31,7 @@ const Home = () => {
       <Navbar />
       <Carousel images={images} />
       <HeroText />
+      <Footer />
     </div>
   );
 };
