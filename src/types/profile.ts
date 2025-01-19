999a3d24058553d5fa779a42de9229beef7f7ec2
Aileen Shi <89747088+aileen-shi@users.noreply.github.com>
2025-01-19 17:43:54 -0500

    Created Profile interface
    Created interface with data for team members

---
+++ src/types/profile.ts
@@ -0,0 +1,7 @@
+export interface Profile {
+  image: string;
+  name: string;
+  background: string;
+  about: string;
+}
+export default Profile;
---
+++ src/types/profilesData.ts
@@ -0,0 +1,51 @@
+import { Profile } from "./profile";
+import Tyreek from "../assets/images/Members/Tyreek.jpg";
+import Shelby from "../assets/images/Members/Shelby.jpg";
+import Emily from "../assets/images/Members/Emily.jpg";
+import Olivia from "../assets/images/Members/Olivia.jpg";
+
+const profiles: Profile[] = [
+  {
+    image: Tyreek,
+    name: "Tyreek Jenkins",
+    background: "Ph.D. Candidate",
+    about:
+      "Tyreek is a Ph.D. candidate in the Molecular and Cellular Biology and  Pathobiology Program at MUSC who is interested in understanding how altered mitochondrial activity in the stria vascularis, the inner ear's  cellular battery, contributes to cochlear inflammation, a hallmark  characteristic of sensorineural hearing loss. As a PhD candidate in the  Lang Lab, Tyreek uses advanced biochemical imaging approaches and  molecular biological techniques to investigate the relationship between  mitochondrial dysfunction and aberrant immune cell activity in various  mouse models of hearing loss.",
+  },
+  {
+    image: Shelby,
+    name: "Shelby Ashton Payne",
+    background: "M.D., Ph.D. Candidate",
+    about:
+      "Shelby Payne is an M.D. / Ph.D. student currently enrolled in the  NIH-funded Medical Scientist Training Program (MSTP) at the MUSC. Shelby holds a Bachelor of Arts degree in Classical Studies with a minor in  Speech and Hearing Sciences from Washington University in St. Louis  which piqued her interest in becoming a physician-scientist and  otolaryngologist. Before joining the Lang Laboratory in 2023, Shelby  studied auditory neuroscience and cochlear inflammation at the  Department of Otolaryngology–Head and Neck Surgery at Washington  University School of Medicine (WUSM) from 2016 to 2021 where she  contributed to numerous publications and presented at national  conferences. She was also invited to the Biology of the Inner Ear Course at the Marine Biological Laboratory in Woods Hole, MA as a research  instructor to teach students patch-clamp electrophysiology. She is  interested in studying the role of the immune system in auditory  pathology with the goal of pioneering novel therapeutics to treat  hearing loss and diseases of the inner ear.",
+  },
+  {
+    image: Emily,
+    name: "Emily Fabrizio-Stover",
+    background: "Ph.D.",
+    about:
+      "After completing my undergraduate degree at the University of  Connecticut, I earned my Ph.D. at the University of Connecticut in the  laboratory of Dr. Douglas Oliver. During my doctorate degree, my  research focused on developing non-invasive, electrophysiological  correlates of tinnitus in both laboratory animals and human subjects. As a postdoc in both the Lang lab and Harris lab, I am interested in how  the excitatory/inhibitory balance in the auditory system changes with  increasing age and what underlying factors contribute to this imbalance.",
+  },
+  {
+    image: Olivia,
+    name: "Olivia Lopez",
+    background: "B.S. MUSC PREP Scholar",
+    about:
+      "Olivia is a scholar in the post-baccalaureate research education program (PREP) for biomedical research at MUSC. She has an interest in  understanding the potential contributions of a high-fat diet to aberrant macrophage activity and chronic cochlear inflammation, which both play a role in the progression of age-related hearing loss. In her free time,  she enjoys baking, reading, and playing musical instruments.",
+  },
+  {
+    image: "",
+    name: "Jiaying Wu",
+    background: "B.S., M.S. Research Specialist",
+    about:
+      "Jiaying joined the Lang Lab in 2022, studying auditory nerve survival  and degeneration. She specializes in animal husbandry, management of  mouse colonies, auditory function measurements, cochlear dissection,  immunohistochemistry staining, and data analysis of degenerative cochlea tissues.",
+  },
+  {
+    image: "",
+    name: "Juhong Zhu",
+    background: "B.S., M.S. Research Specialist",
+    about:
+      "Junhong has been responsible for tissue processing and sectioning, and  immunostaining assays for high-resolution confocal imaging of the  cochlea and brain (including both mouse and human specimens",
+  },
+];
+export default profiles;
